20872d35a666e527ab27b5cde3ef2a8409b197bf
Victor Raúl García García <victor07mejor@gmail.com>
2025-11-24 18:43:35 -0600

    ref: modifica el flujo de carpetas para suar ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button } from "./components/ui/button";
+import { Button } from "./ui/components/ui/button";
 
 export const App = () => {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App.tsx";
-import { ThemeProvider } from "./components/theme-provider.tsx";
+import { ThemeProvider } from "./ui/components/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
